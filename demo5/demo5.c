44ad70b255831979202f8f143ffafde32aabef71
Zzx <472320933@qq.com>
2023-03-23 21:28:39 +0800

    modified:   demo4/demo4.c 	deleted:    demo4/demo4.exe 	modified:   demo5/demo5.c 	new file:   demo5/demo5.exe

--- demo5/demo5.c
+++ demo5/demo5.c
@@ -2,8 +2,20 @@
 
 /* *****指针****** */
 
-/*  */
-int mian()
+/* 求字符串长度 */
+int my_strlen(char *str)
 {
+    char *start = str;
+    while (*str != '\0')
+    {
+        str++;
+    }
+    return str - start;
+}
+
+int main()
+{
+    int len = my_strlen("abc");
+    printf("%d\n", len);
     return 0;
 }
