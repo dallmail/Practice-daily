e0f4313c4963e3adef5ade19ac439c1f7f64296e
Zzx <472320933@qq.com>
2023-03-25 16:49:42 +0800

    modified:   demo5/demo5.c 	modified:   demo5/demo5.exe 	new file:   project01/demo1.c 	new file:   project01/demo1.exe

--- demo5/demo5.c
+++ demo5/demo5.c
@@ -3,19 +3,25 @@
 /* *****指针****** */
 
 /* 求字符串长度 */
-int my_strlen(char *str)
-{
-    char *start = str;
-    while (*str != '\0')
-    {
-        str++;
-    }
-    return str - start;
-}
+// int my_strlen(char *str)
+// {
+//     char *start = str;
+//     while (*str != '\0')
+//     {
+//         str++;
+//     }
+//     return str - start;
+// }
 
+// int main()
+// {
+//     int len = my_strlen("abc");
+//     printf("%d\n", len);
+//     return 0;
+// }
+
+/*  */
 int main()
 {
-    int len = my_strlen("abc");
-    printf("%d\n", len);
     return 0;
 }
---
+++ project01/demo1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**********C语言真题编程题 ***********/
+/* 1. 求 n 的阶乘。 */
+// int main()
+// {
+//     int n = 0;
+//     int num = 1;
+//     printf("请输入n来求阶乘：");
+//     scanf("%d", &n);
+//     for (int i = 1; i <= n; i++)
+//     {
+//         num *= i;
+//     }
+//     printf("!n=%d\n", num);
+//     return 0;
+// }
+
+/* 2. 判断一个数是否为素数。 */
+int main()
+{
+
+    return 0;
+}
