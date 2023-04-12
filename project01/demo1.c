5168d97a14829c48d3991670cc4e79a6482dff35
Zzx <472320933@qq.com>
2023-04-12 16:25:29 +0800

    modified:   project01/demo1.c 	modified:   project01/demo1.exe

--- project01/demo1.c
+++ project01/demo1.c
@@ -16,8 +16,45 @@
 // }
 
 /* 2. 判断一个数是否为素数。 */
-int main()
-{
+// int main()
+// {
+//     int a, i;
+//     printf("请输入需判断的数->");
+//     scanf("%d", &a);
+//     for (i = 2; i < a; i++)
+//     {
+//         if (a % i == 0)
+//             break;
+//     }
+//     if (i < a)
+//         printf("%d不是素数\n", a);
+//     else
+//         printf("%d是素数\n", a);
+//     return 0;
+// }
 
-    return 0;
-}
+/* 有序数组，输入两个数，从第几个数开始倒序几个数，打印 */
+// #define N 10
+// int main()
+// {
+//     int a, b;
+//     int arr[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//     scanf("%d%d", &a, &b);
+//     int first = a - 1;
+//     int end = first + b - 1;
+//     for (int i = 0; i < N; i++)
+//     {
+//         if (first < end)
+//         {
+//             int temp = arr[first];
+//             arr[first] = arr[end];
+//             arr[end] = temp;
+//             first++;
+//             end--;
+//         }
+//         printf("%2d ", arr[i]);
+//     }
+//     return 0;
+// }
+
+/*  */
